64c53dd76b702d60dc685cf30cc23ce43088c1d8
sasconsul sas1@uchicago.edu <sas1@uchicago.edu>
2019-11-22 17:10:54 -0800

    Initial commit of generated stub code.

---
+++ src/main/java/net/sasconul/funWithFlags/FunWithFlagsApplication.java
@@ -0,0 +1,13 @@
+package net.sasconul.funWithFlags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithFlagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunWithFlagsApplication.class, args);
+	}
+
+}
